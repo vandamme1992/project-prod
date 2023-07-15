646021d285cfaf50f87aa88f6d0c0d275007fc16
vandamme1992 <maksim.klisov21@gmail.com>
2023-07-15 21:27:02 +0300

    init commit

---
+++ src/app/App.tsx
@@ -0,0 +1,19 @@
+import './styles/index.scss';
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/NavBar";
+
+
+const App = () => {
+const {theme} = useTheme()
+
+    return (
+        <div className={classNames('app', {},[theme])}>
+            <Navbar/>
+        <AppRouter/>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/widgets/NavBar/ui/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './Navbar.module.scss';
+import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
+
+
+interface NavbarProps {
+className?: string;
+}
+
+export const Navbar = ({className}: NavbarProps) => {
+    return (
+        <div className={classNames(cls.Navbar,{}, [className])}>
+            <ThemeSwitcher/>
+            <div className={cls.links}>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/'}>Главная страница</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>О сайте</AppLink>
+            </div>
+        </div>
+    );
+};
+
+
+
