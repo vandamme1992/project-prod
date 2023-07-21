c30a8e6ec207f5d1ef32082bf808faeab38ca236
vandamme1992 <maksim.klisov21@gmail.com>
2023-07-21 22:18:26 +0300

    internationalization

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,19 +4,21 @@ import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/NavBar";
 import {Sidebar} from "widgets/Sidebar/ui/Sidebar/Sidebar";
-
+import { Suspense } from 'react';
 
 const App = () => {
 const {theme} = useTheme()
 
     return (
         <div className={classNames('app', {},[theme])}>
+            <Suspense fallback={<div>Loading...</div>}>
             <Navbar/>
+
             <div className={'content-page'}>
                 <Sidebar/>
                 <AppRouter/>
             </div>
-
+            </Suspense>
         </div>
     );
 };
