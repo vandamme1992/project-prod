514b0979ed8d61e6ef0480a7f3720671239c320b
vandamme1992 <maksim.klisov21@gmail.com>
2023-09-01 00:06:39 +0300

    add Storybook

--- src/pages/AboutPage/ui/AboutPage.async.tsx
+++ src/pages/AboutPage/ui/AboutPage.async.tsx
@@ -1,3 +1,7 @@
 import { lazy } from 'react';
 
-export const AboutPageAsync = lazy(() => import('./AboutPage'));
+export const AboutPageAsync = lazy(() => new Promise((resolve) => {
+    // @ts-ignore
+    // Так не можно робити в реальному проекті!
+    setTimeout(() => resolve(import('./AboutPage')), 1500);
+}));
