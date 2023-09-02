3dd15152495062987d3020206a3ee88134e58c76
vandamme1992 <maksim.klisov21@gmail.com>
2023-09-02 21:30:22 +0300

    fix className unit test

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -1,12 +1,14 @@
-import { classNames } from './classNames';
+import { classNames } from 'shared/lib/classNames/classNames';
 
 describe('classNames', () => {
     test('with only first param', () => {
         expect(classNames('someClass')).toBe('someClass');
     });
+
     test('with additional class', () => {
         const expected = 'someClass class1 class2';
-        expect(classNames('someClass', {}, ['class1', 'class2'])).toBe(expected);
+        expect(classNames('someClass', {}, ['class1', 'class2']))
+            .toBe(expected);
     });
 
     test('with mods', () => {
@@ -19,15 +21,16 @@ describe('classNames', () => {
     });
 
     test('with mods false', () => {
-        const expected = 'someClass class1 class2 hovered scrollable';
+        const expected = 'someClass class1 class2 hovered';
         expect(classNames(
             'someClass',
             { hovered: true, scrollable: false },
             ['class1', 'class2'],
         )).toBe(expected);
     });
+
     test('with mods undefined', () => {
-        const expected = 'someClass class1 class2 hovered scrollable';
+        const expected = 'someClass class1 class2 hovered';
         expect(classNames(
             'someClass',
             { hovered: true, scrollable: undefined },
